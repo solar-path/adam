9099ba0b6850c6959a4b113439d7ed028d365634
solar-path <55182918+solar-path@users.noreply.github.com>
2025-03-07 22:09:50 +0500

    setup web server

--- go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/solar-path/adam
-
-go 1.24.1
-
-require (
-	github.com/gorilla/mux v1.8.1
-	github.com/lib/pq v1.10.9
-)
--- main.go
+++ main.go
@@ -1,57 +1,22 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
-	_ "github.com/lib/pq"
 )
 
-// declare database variable
-var db *sql.DB
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to the Home page")
+}
 
-// initilize database connection
-func initDB() {
-	// error variable
-	var err error
-	// supabase url
-	supabaseURL := "postgres://postgres:zFAYvTAl1Zu1Vqvk@aws-0-eu-central-1.pooler.supabase.com:6543/postgres?options=reference%3Dxizxaxgkhimeipbokvqf"
-	// check if supabase url is empty
-	if supabaseURL == "" {
-		log.Fatal("SUPABASE_URL environment variable is required")
-	}
-
-	// open database connection - define driver name as postgres and connection string as supabaseURL
-	db, err = sql.Open("postgres", supabaseURL)
-	// check for errors
-	if err != nil {
-		log.Fatal("Error connecting to database:", err)
-	}
-
-	// ping database
-	err = db.Ping()
-	if err != nil {
-		log.Fatal("Error pinging database:", err)
-	}
-
-	fmt.Println("Successfully connected to Supabase Postgres")
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
 }
 
 func main() {
-	initDB()
-	defer db.Close()
-
-	gRouter := mux.NewRouter()
-	gRouter.HandleFunc("/", HomeHandler)
-
-	port := ":3000"
-	fmt.Printf("Server starting on port %s\n", port)
-	log.Fatal(http.ListenAndServe(port, gRouter))
-}
-
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome to the Home page")
+	setupRoutes()
+	fmt.Println("Server started at 127.0.0.1:3000")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		fmt.Println("Failed to start server:", err)
+	}
 }
