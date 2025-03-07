64a56c10dc080c8bcd52996a24f0b93ef1580519
solar-path <55182918+solar-path@users.noreply.github.com>
2025-03-08 00:45:33 +0500

    server index.html in hono

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,14 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 app.use(logger());
 
-app.get("/", (c) => c.text("Hello via Bun!"));
+// Serve static files
+app.use("/static/*", serveStatic({ root: "./public" }));
+
+// Serve index.html at root
+app.get("/", serveStatic({ path: './public/index.html'}));
 
 export default app;
