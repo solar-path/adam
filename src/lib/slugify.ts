04922b353852731dc7c80981200246c6fa944e2b
solar-path <55182918+solar-path@users.noreply.github.com>
2025-03-09 12:03:47 +0500

    new project

--- src/lib/slugify.ts
+++
@@ -1,9 +0,0 @@
-export const slugify = async (base: string) => {
-    return base
-      .trim()
-      .toLowerCase()
-      .replace(/\s+/g, "-")
-      .replace(/[^a-z0-9-]/g, "")
-      .replace(/-+/g, "-")
-      .replace(/(^-|-$)/g, "");
-  };
