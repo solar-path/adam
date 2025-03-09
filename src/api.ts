7680aa9ca1efeb21986206ab9aefe9c49f9a958e
solar-path <55182918+solar-path@users.noreply.github.com>
2025-03-09 23:54:42 +0500

    place index.html to ./public

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
-
+import { serveStatic } from "hono/bun";
 const app = new Hono();
 
 const TodoSchema = z.object({
@@ -37,7 +37,7 @@ const todoList: Todo[] = [
 
 app.use(logger())
 
-app.get("/", (c) => c.text("Hello World"))
+app.get("/*", serveStatic({ root: './public' }))
 
 // Get all todos
 app.get('/todo', (c) => c.json({
